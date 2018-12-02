6cf29e21943b53b04037fc28656e5abe963cd8de
User-PC\User <hungphat11997@gmail.com>
2018-12-02 17:10:11 +0700

    init layout

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import NavBar from './NavBar';
 import { Grid, Row, Col } from 'react-bootstrap';
 import Image from 'react-image-resizer';
+import { connect } from 'react-redux';
+import { updateCoverImage } from '../actions/updateCoverImage';
+import {updateTab } from '../actions/updateTab';
+import { bindActionCreators } from 'redux';
 class HomePage extends Component {
   render() {
     return (
@@ -9,66 +13,73 @@ class HomePage extends Component {
         <div>
             <div class="cover-div">
             <img class="cover-image"
-          src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Monasterio_Khor_Virap%2C_Armenia%2C_2016-10-01%2C_DD_25.jpg/1200px-Monasterio_Khor_Virap%2C_Armenia%2C_2016-10-01%2C_DD_25.jpg"
+          src={this.props.coverImage.cover}
         />
         </div>
-      <NavBar/>
+      <NavBar tab={this.props.tab} onUpdateTab={this.props.onUpdateTab}/>
       
       <Row className="show-grid">
-        <Col xs={6} md={4}>
+        <Col xs={6} md={3}>
         <div class="col-space user-info">
          <p> Name: ABC</p>
          <p> Age: 20</p>
          <p> Phone: 123456</p>
           </div>
         </Col>
-        <Col xs={6} md={4}>
+        <Col xs={6} md={5}>
         <div class="col-space">
           <ul>
               <li class="list-li">
               <Row className="show-grid">
-              <Col xs={6} md={2}>
+              <Col xs={6} md={1}>
               <img class="user-image" src="https://4.bp.blogspot.com/-MrZt66Yr1TE/W2GLo95RU5I/AAAAAAABppo/d0-_hQ5ePcQrLje3PmIwhQmf_MeZDSkOACLcBGAs/s1600/champions-league-ball-2018-2019%2B%25282%2529.jpg"/>
               </Col>
-              <Col xs={6} md={10}>
+              
+              <Col xs={6} md={11}>
+              <div class="post-space">
               <p class="post-name">ABC</p>
               <p>This is my 1st post</p>
               <Image width={300} height={200} src="https://4.bp.blogspot.com/-MrZt66Yr1TE/W2GLo95RU5I/AAAAAAABppo/d0-_hQ5ePcQrLje3PmIwhQmf_MeZDSkOACLcBGAs/s1600/champions-league-ball-2018-2019%2B%25282%2529.jpg"/>
               <i class="fa fa-comments-o icon-size cmt-icon"> 23</i>
               <i class="fa fa-share-alt icon-size share-icon">10</i>
               <i class="fa fa-heart-o icon-size like-icon">50</i>
+              </div>
               </Col>
               </Row>
               </li>
               <br/>
               <li class="list-li">
               <Row className="show-grid">
-              <Col xs={6} md={2}>
+              <Col xs={6} md={1}>
               <img class="user-image" src="https://4.bp.blogspot.com/-MrZt66Yr1TE/W2GLo95RU5I/AAAAAAABppo/d0-_hQ5ePcQrLje3PmIwhQmf_MeZDSkOACLcBGAs/s1600/champions-league-ball-2018-2019%2B%25282%2529.jpg"/>
               </Col>
-              <Col xs={6} md={10}>
+              <Col xs={6} md={11}>
+              <div class="post-space">
               <p class="post-name">ABC</p>
               <p>This is my 1st post</p>
               <Image width={300} height={200} src="https://4.bp.blogspot.com/-MrZt66Yr1TE/W2GLo95RU5I/AAAAAAABppo/d0-_hQ5ePcQrLje3PmIwhQmf_MeZDSkOACLcBGAs/s1600/champions-league-ball-2018-2019%2B%25282%2529.jpg"/>
               <i class="fa fa-comments-o icon-size cmt-icon"> 23</i>
               <i class="fa fa-share-alt icon-size share-icon">10</i>
               <i class="fa fa-heart-o icon-size like-icon">50</i>
+              </div>
               </Col>
               </Row>
               </li>
               <br/>
               <li class="list-li">
               <Row className="show-grid">
-              <Col xs={6} md={2}>
+              <Col xs={6} md={1}>
               <img class="user-image" src="https://4.bp.blogspot.com/-MrZt66Yr1TE/W2GLo95RU5I/AAAAAAABppo/d0-_hQ5ePcQrLje3PmIwhQmf_MeZDSkOACLcBGAs/s1600/champions-league-ball-2018-2019%2B%25282%2529.jpg"/>
               </Col>
-              <Col xs={6} md={10}>
+              <Col xs={6} md={11}>
+              <div class="post-space">
               <p class="post-name">ABC</p>
               <p>This is my 1st post</p>
               <Image width={300} height={200} src="https://4.bp.blogspot.com/-MrZt66Yr1TE/W2GLo95RU5I/AAAAAAABppo/d0-_hQ5ePcQrLje3PmIwhQmf_MeZDSkOACLcBGAs/s1600/champions-league-ball-2018-2019%2B%25282%2529.jpg"/>
               <i class="fa fa-comments-o icon-size cmt-icon"> 23</i>
               <i class="fa fa-share-alt icon-size share-icon">10</i>
               <i class="fa fa-heart-o icon-size like-icon">50</i>
+              </div>
               </Col>
               </Row>
               </li>
@@ -88,4 +99,17 @@ class HomePage extends Component {
   }
 }
 
-export default HomePage;
+const mapStateToProps = (state) => {
+  return {
+    coverImage: state.coverImage,
+    tab: state.tab,
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({
+    onUpdateCoverImage: updateCoverImage,
+    onUpdateTab: updateTab
+  }, dispatch);
+}
+export default connect(mapStateToProps, mapDispatchToProps) (HomePage);
--- src/index.js
+++ src/index.js
@@ -3,8 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider} from 'react-redux';
+import { createStore, combineReducers } from 'redux';
+import coverImageReducer from './reducers/coverimage-reducer';
+import tabReducer from './reducers/tab-reducer';
+const rootReducer = combineReducers({
+    coverImage: coverImageReducer,
+    tab: tabReducer,
+  })
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Create store with reducers
+
+const store = createStore(rootReducer)
+ReactDOM.render(<Provider store={store}>
+    <App/>
+    </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
