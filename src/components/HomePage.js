c9f174247e2501b354af8d17b3fd2bdfe5af67a8
hungphat <39639653+hungphat11997@users.noreply.github.com>
2018-12-04 08:20:27 +0700

    update Homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -171,14 +171,21 @@ class HomePage extends Component {
               <Row className="show-grid">
               <Col xs={6} md={1}>
               <img class="user-image" src="https://4.bp.blogspot.com/-MrZt66Yr1TE/W2GLo95RU5I/AAAAAAABppo/d0-_hQ5ePcQrLje3PmIwhQmf_MeZDSkOACLcBGAs/s1600/champions-league-ball-2018-2019%2B%25282%2529.jpg"/>
-
               </Col>
-              <Col xsHidden md={4}>
-                <div class="col-space">
-                  <code>{'<Col xsHidden md={4} />'}</code>
-                </div>
+              <Col xs={6} md={11}>
+              <div class="post-space">
+              <p class="post-name">ABC</p>
+              <p>This is my 1st post</p>
+              <Image width={300} height={200} src="https://4.bp.blogspot.com/-MrZt66Yr1TE/W2GLo95RU5I/AAAAAAABppo/d0-_hQ5ePcQrLje3PmIwhQmf_MeZDSkOACLcBGAs/s1600/champions-league-ball-2018-2019%2B%25282%2529.jpg"/>
+              <i class="fa fa-comments-o icon-size cmt-icon"> 23</i>
+              <i class="fa fa-share-alt icon-size share-icon">10</i>
+              <i class="fa fa-heart-o icon-size like-icon">50</i>
+              </div>
               </Col>
-            </Row>
+              </Row>
+              </li>
+              <br/>
+          </ul>
           </div>
         </Col>
         <Col xsHidden md={4}>
